e316c838257184580845f2644762b8aebc0e7243
Shagun Garg <shagun1garg@gmail.com>
2021-10-31 15:51:27 -0500

    Start a Project

---
+++ cmake-build-debug/CMakeFiles/TheCaeserDecrypter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TheCaeserDecrypter.dir/main.cpp.o"
+  "TheCaeserDecrypter"
+  "TheCaeserDecrypter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TheCaeserDecrypter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
